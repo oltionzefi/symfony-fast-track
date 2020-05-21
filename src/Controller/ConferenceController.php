7486861dfab22daaaa0e2aa0f11deede9a16aa8d
Oltion Zefi <oltionzefi@gmail.com>
2020-05-21 13:50:23 +0200

    Change CommentForm

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -10,15 +10,21 @@ use Twig\Environment;
 use App\Repository\ConferenceRepository;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ConferenceController extends AbstractController
 {
     private $twig;
 
+    private $entityManager;
 
-    public function __construct(Environment $twig)
+
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
     /**
      * @Route("/", name="homepage")
@@ -38,13 +44,28 @@ class ConferenceController extends AbstractController
         Conference $conference, 
         CommentRepository $commentRepository
     ) {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView()
         ]));
     }
 }
